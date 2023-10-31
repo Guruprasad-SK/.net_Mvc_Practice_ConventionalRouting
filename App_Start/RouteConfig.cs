10756373fb0852bbcef0ff515dbec64286ddb2e9
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-10-31 23:28:07 +0530

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ConventionalRouting
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "allStudent",
+               url: "student",
+               defaults: new { controller = "Student", action = "GetAllStudent" });
+
+            routes.MapRoute(
+               name: "Student",
+               url: "student/{id}",
+               defaults: new { controller = "Student", action = "GetStudentId" });
+
+            routes.MapRoute(
+               name: "StudentAddress",
+               url: "student/{id}/Address",
+               defaults: new { controller = "Student", action = "GetStudentAddress" });
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConventionalRouting.Models;
+
+namespace ConventionalRouting.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult GetAllStudent()
+        {
+            var students = Students();
+            return View(students);
+        }
+        public ActionResult GetStudentId(int id)
+        {
+            var student = Students().FirstOrDefault(x => x.Id == id);
+            return View(student);
+        }
+        public ActionResult GetStudentAddress(int id)
+        {
+            var student = Students().Where(y => y.Id == id).Select(x => x.Address).FirstOrDefault();
+            return View(student);
+        }
+        private List<Student> Students()
+        {
+            return new List<Student>()
+            {
+                new Student()
+                {
+                    Id=1,
+                    Name="Student1",
+                    Class="class 1",
+                    Address=new Address()
+                    {
+                        HomeNumber="student 1",
+                        Address1="student 1 address",
+                        City="student 1 city"
+
+
+                    }
+
+
+                },
+                new Student()
+                {
+                    Id=2,
+                    Name="Student2",
+                    Class="class 2",
+                    Address=new Address()
+                    {
+                        HomeNumber="student 2",
+                        Address1="student 2 address",
+                        City="student 2 city"
+
+
+                    }
+
+
+                },
+                new Student()
+                {
+                    Id=3,
+                    Name="Student3",
+                    Class="class 3",
+                    Address=new Address()
+                    {
+                        HomeNumber="student 3",
+                        Address1="student 3 address",
+                        City="student 3 city"
+
+
+                    }
+
+
+                },
+                new Student()
+                {
+                    Id=4,
+                    Name="Student4",
+                    Class="class 4",
+                    Address=new Address()
+                    {
+                        HomeNumber="student 4",
+                        Address1="student 4 address",
+                        City="student 4 city"
+
+
+                    }
+
+
+                },
+                new Student()
+                {
+                    Id=5,
+                    Name="Student5",
+                    Class="class 5",
+                    Address=new Address()
+                    {
+                        HomeNumber="student 5",
+                        Address1="student 5 address",
+                        City="student 5 city"
+
+
+                    }
+
+
+                },
+                new Student()
+                {
+                    Id=6,
+                    Name="Student6",
+                    Class="class 6",
+                    Address=new Address()
+                    {
+                        HomeNumber="student 6",
+                        Address1="student 6 address",
+                        City="student 6 city"
+
+
+                    }
+
+
+                },
+
+            };
+        }
+
+
+    }
+}
